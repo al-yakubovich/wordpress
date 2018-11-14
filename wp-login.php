83fcee8caa7cabd700b2821c57cd1b31551f3f0a
Alexander <al.yakubovich@yahoo.com>
2018-11-14 20:30:37 +0300

    14nov18

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; blog &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='http://yakubovich.net/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='https://yakubovich.net/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -17,7 +17,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="http://yakubovich.net/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://yakubovich.net/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -29,13 +29,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="http://yakubovich.net/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://yakubovich.net/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="http://yakubovich.net/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="https://yakubovich.net/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -52,7 +52,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://yakubovich.net/">&larr; Back to blog</a></p>
+	<p id="backtoblog"><a href="https://yakubovich.net/">&larr; Back to blog</a></p>
 		
 	</div>
 
